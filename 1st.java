39f247977d96a5808186980b1a62713e62944e8f
Ansh <117647423+AnshKumar6400@users.noreply.github.com>
2025-06-05 12:54:44 +0530

    2nd.java

--- 1st.java
+++ 1st.java
@@ -1,29 +1,31 @@
-// Online Java Compiler
-// Use this editor to write, compile and run your Java code online
 import java.util.*;
+
 class Main {
     public static void main(String[] args) {
-       Scanner sc=new Scanner(System.in);
-     
-       int n=sc.nextInt();
-       int nums[]=new int [n];
-       for(int i=0;i<n;i++){
-           nums[i]=sc.nextInt();
-       }
-   
-       int res=0;
-       int curr=0;
-       for(int i=1;i<nums.length;i++){
-           System.out.println(nums[i]);
-           int prev=nums[i-1];
-           if(prev<nums[i]){
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int nums[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        if (n == 0) {
+            System.out.println(0);
+            return;
+        }
+
+        int res = 1;
+        int curr = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i - 1] < nums[i]) {
                 curr++;
-               
-           }else{
-              res=Math.max(res,curr);
-               curr=0;
-           }
-       }
-       System.out.println(res);
+            } else {
+                res = Math.max(res, curr);
+                curr = 1;
+            }
+        }
+        res = Math.max(res, curr);
+        System.out.println(res);
     }
 }
