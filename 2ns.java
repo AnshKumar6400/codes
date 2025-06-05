1b53cefa1b38dd7533278b3a8b4d2454be2785f1
Ansh <117647423+AnshKumar6400@users.noreply.github.com>
2025-06-05 12:56:12 +0530

    Update 1st.java

--- 2ns.java
+++ 2ns.java
@@ -1,30 +1,32 @@
-// Online Java Compiler
-// Use this editor to write, compile and run your Java code online
 import java.util.*;
+
 class Main {
-    public static boolean ispalin(String s){
-        int i=0;
-        int j=s.length();
-        while(i<j){
-            if(s.charAt(i)!=s.charAt(j)){
+    public static boolean ispalin(String s) {
+        int i = 0;
+        int j = s.length() - 1;
+        while (i < j) {
+            if (s.charAt(i) != s.charAt(j)) {
                 return false;
             }
             i++;
             j--;
-            
         }
         return true;
     }
+
     public static void main(String[] args) {
-  Scanner sc=new Scanner(System.in);
-String s=sc.nextLine();
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
 
-String res="";
-for(int i=0;i<s.length();i++){
-if(s.charAt(i)<'9'&&s.charAt(i)>='0')res+=s.charAt(i);
-if(s.charAt(i)>'A'&&s.charAt(i)<'Z')res+=s.charAt(i)-'A'+'a';
-}
+        String res = "";
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c >= '0' && c <= '9') res += c;
+            else if (c >= 'A' && c <= 'Z') res += (char)(c + 32);
+            else if (c >= 'a' && c <= 'z') res += c;
+        }
 
-System.out.println(res);
+        System.out.println(res);
+        System.out.println(ispalin(res));
     }
 }
